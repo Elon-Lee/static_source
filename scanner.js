eabe10c321bf9feb83922e37715c893201a112ba
LiYunTao <35645739+Elon-Lee@users.noreply.github.com>
2025-01-12 12:57:28 +0800

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,18 +1,32 @@
-
 (function() {
-    // 处理 API 响应的 body
-   
      // 处理 API 响应的 body
-    var body = $response.body;
-    var body_orginal = body
-    // 替换相关字段
-     
+    var body = JSON.parse($response.body);  
+    body.data.psnl_vip_property = {"renew_method": "appstore",
+      "initial_tm": "1614867690",
+      "svip": 1,
+      "auto_renewal": true,
+      "ms_first_pay": 0,
+      "pending": 0,
+      "group2_paid": 0,
+      "inherited_flag": 0,
+      "nxt_renew_tm": "9915126887",
+      "level_info": {
+        "level": 1,
+        "days": 1,
+        "end_days": 30
+      },
+      "group1_paid": 1,
+      "ys_first_pay": 0,
+      "renew_type": "year",
+      "expiry": 8487890487,
+      "grade": 2,
+      "last_payment_method": "appstore",
+      "product_id": "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low"}
     // 返回修改后的 body
     var response = {
-        body: body
+        body: JSON.stringify(body)
     };
-    console.log(`scanner before modify : ${body_orginal} response body: ${body}`)
-
+    console.log(`scanner after response  : ${JSON.stringify(body)} `)
     $done(response);
     
 })();
