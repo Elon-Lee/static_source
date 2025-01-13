cf6532e4faf28b63a19e237d4021f17af5e52c74
LiYunTao <35645739+Elon-Lee@users.noreply.github.com>
2025-01-13 21:29:19 +0800

    Create xinbiaozhun_english.js

---
+++ xinbiaozhun_english.js
@@ -0,0 +1,15 @@
+
+(function() { 
+    // 处理 API 响应的 body
+    var body = $response.body;
+    var body_orginal = body
+    // 替换相关字段
+    body = body.replace(/\"hasPurchase":false/g, '"hasPurchase":true') // 修改会员状态为 1
+               .replace("2021-09-20", '2099-09-20') // 设置用户名
+    // 返回修改后的 body
+    var response = {
+        body: body
+    };
+    console.log(`xinkebiao_english after modifyresponse body: ${body}`)
+    $done(response);
+})();
